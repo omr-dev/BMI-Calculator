4b9186642cee1d9663ea12ef4f8f355abe77f6bc
omer <peynirciomerfaruk@gmail.com>
2022-02-15 10:34:38 +0100

    part 3 completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const inHeight=Document.getIt
+const resultDiv = document.getElementById("resultDiv");
+const BMIresult = document.getElementById("BMIresult");
+resultDiv.style.display = "none";
+
+const printBMI = () => {
+  const height = document.getElementById("inHeight").value;
+  const weight = document.getElementById("inWeight").value;
+  const calculatedBMI = calculateBMI(height, weight);
+  BMIresult.innerHTML = calculatedBMI;
+  //const resultDiv = Document.getItemById("resultDiv");
+  console.log("height", height, "weight", weight);
+  resultDiv.style.display = "block";
+};
+const calculateBMI = (height, weight) => {
+  return weight * ((height / 100) ** 2).toFixed(1);
+};
